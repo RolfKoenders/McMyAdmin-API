1d44046640af1ed297508f79ed4d263011b8fee0
Rolf Koenders <post@madebyrolf.nl>
2016-12-27 16:40:10 +0100

    Add project files

---
+++ gulpfile.js
@@ -0,0 +1,25 @@
+'use strict';
+const gulp = require('gulp');
+const runSequence = require('run-sequence');
+const xo = require('gulp-xo');
+const ava = require('gulp-ava');
+
+gulp.task('default', callback => {
+	runSequence([
+		'lint',
+		'test'
+	], callback);
+});
+
+gulp.task('lint', () => {
+	return gulp.src([
+		'gulpfile.js',
+		'lib/*.js'
+	]).pipe(xo({quiet: true}));
+});
+
+gulp.task('test', () => {
+	return gulp.src([
+		'test/unit/**/*.js'
+	]).pipe(ava({verbose: true}));
+});
